32399c2842de512c3ced2e03eda0fdb57dcfcffd
Mourad0906 <m.mouradchikhi@gmail.com>
2024-11-06 15:19:56 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    /**
+     * Calcule la factorielle d'un entier positif
+     * @param x;
+     * @return x * Fact(x-1)
+     */
+    public static int Fact(int x){
+        if(x==1){
+            return 1;
+        }else{
+            return x * Fact(x-1);
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
